06e326fd4474a51661f3555e245d164efddb0ed0
praveenr-tiltlabs <59870131+cryptedveen@users.noreply.github.com>
2023-02-10 04:52:51 +0530

    Timer Added

---
+++ Assets/CountdownTimer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountdownTimer : MonoBehaviour
+{
+    [SerializeField] float TotalTime = 60;
+    int seconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TotalTime > 0)
+
+        {
+            TotalTime -= Time.deltaTime;
+            seconds = Mathf.FloorToInt(TotalTime % 60);
+
+            gameObject.GetComponent<TextMeshProUGUI>().text = seconds.ToString();
+        }
+
+
+
+    }
+}
